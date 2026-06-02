f5d45bfaa666a55ff9663433681cd4f707033fcb
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-02 20:18:48 -0300

    theme: re-export getTileStyles and all style maps

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,3 @@
-export { tileStyles } from './tileStyles'
-export type { TileStyle } from './tileStyles'
+export { getTileStyles, tileStylesDark, tileStylesLight, overlayStyles, editorStyles } from './tileStyles'
+export type { TileStyle, OverlayStyle, EditorStyleExtension } from './tileStyles'
 import './vars.css'
